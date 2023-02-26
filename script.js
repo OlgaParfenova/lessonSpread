97313a096c742f0aa394f639a832603d9d5c1f3d
Olga Parfenova <work.parfenova@gmail.com>
2023-02-26 23:30:26 +0300

    The first module is completed

---
+++ script.js
@@ -0,0 +1,29 @@
+// task 1
+
+// let arr = [1, 2, 3, 4, 5];
+//
+// function func(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+//
+// console.log(func(...arr));
+
+// task 2
+
+// let arr = [3, 8, 12, 5, 9, 2];
+//
+// let min = Math.min(...arr);
+//
+// console.log(min);
+
+// task 3
+
+// function getMean(...numbers) {
+//     let sum = 0;
+//     for (let num of numbers) {
+//         sum += num;
+//     }
+//     return sum / numbers.length;
+// }
+//
+// console.log(getMean(7, 3, 1, 6, 12));
